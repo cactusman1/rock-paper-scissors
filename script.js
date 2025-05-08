16dec410e037d9a9432c10a42aa96941f78532ad
cactusman1 <shadys340@gmail.com>
2025-05-08 20:27:55 +0500

    Define how a single round will be played
    I wrote how a single round will be played out.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+    let humanScore = 0;
+    let computerScore = 0;
         // This function will generate the computer's choice. T
         // The idea is that this function will assign (to the "choice variable")
         //  and return either rock paper or scissor based on the value returned by Math.random()
@@ -16,6 +17,61 @@
             }
             return choice;
         }
-        computerChoice = getComputerChoice();
-        console.log(computerChoice);
+        let computerSelection = getComputerChoice();
+        console.log("Computer choice: " + computerSelection);
+
+        function getHumanChoice () {
+            let humanChoice = prompt("CHOOSE!!! rock, paper or scissors?").toUpperCase();
+            let choice;
+            
+            if (humanChoice === "ROCK"){
+                 choice = 'rock';
+            }
+            else if (humanChoice === "PAPER"){
+                choice = 'paper';
+            }
+            else{
+                choice = 'scissors';
+            }
+            return choice;
+        }
+
+        let humanSelection = getHumanChoice();
+        console.log("Your choice: " + humanSelection);
+
+        function playRound (humanChoice, computerChoice){
+
+            if (humanChoice == "rock" && computerChoice == "paper"){
+                console.log("You lose. Paper beats rock.")
+                computerScore++;
+            }
+            else if (humanChoice == "paper" && computerChoice == "scissors") {
+                console.log("You lose. Scissors beats paper.")
+                computerScore++;
+            }
+            else if (humanChoice == "scissors" && computerChoice == "rock") {
+                console.log("You lose. Rock beats scissors.")
+                computerScore++;
+            }
+            else if (humanChoice == computerChoice ){
+                console.log("Draw.")
+            }
+            else{
+                console.log("You win!!!");
+                humanScore++;
+            }
+
+            localScopeFinalScore = `You: ${humanScore}  Computer: ${computerScore}`;
+            
+            return localScopeFinalScore;
+        }
+
+        let finalScore = playRound(humanSelection,computerSelection);
+        console.log(finalScore);
+
+        function playGame () {
+            
+        }
+
+
     
