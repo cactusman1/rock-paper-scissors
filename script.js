24c9871ae9a64e2167e4ee3a150ec66b3b278ed2
cactusman1 <shadys340@gmail.com>
2025-07-19 01:27:30 +0500

    Do something I got the project working how odin project wanted it. Still some tweaks and fixes to be made.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let humanScore = 0;
 let computerScore = 0;
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener('click', () => {
+    playRound(getHumanChoice('ROCK'), getComputerChoice())
+});
+paper.addEventListener('click', () => {
+    playRound(getHumanChoice('PAPER'), getComputerChoice())
+});
+scissors.addEventListener('click', () => {
+    playRound(getHumanChoice('SCISSORS'), getComputerChoice())
+});
+
+
 // This function will generate the computer's choice. T
 // The idea is that this function will assign (to the "choice variable")
 //  and return either rock paper or scissor based on the value returned by Math.random()
@@ -39,37 +55,53 @@ function getHumanChoice(humanChoice) {
     return choice;
 }
 //This function simulates a single round.
+
 function playRound(humanChoice, computerChoice) {
-        const display = document.querySelector(".output");
+    const display = document.querySelector(".output");
 
     if (humanChoice == "rock" && computerChoice == "paper") {
-        display.textContent = "You lose. Paper beats rock.";
         computerScore++;
     }
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        display.textContent = "You lose. Scissors beats paper.";
         computerScore++;
     }
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-        display.textContent = "You lose. Rock beats scissors.";
         computerScore++;
     }
     else if (humanChoice == computerChoice) {
         display.textContent = "Draw.";
     }
     else {
-        display.textContent = "You win!!!";
         humanScore++;
     }
 
     localScopeFinalScore = `You: ${humanScore}  Computer: ${computerScore}`;
+    const scoreBoard = document.querySelector(".scoreBoard");
+    scoreBoard.textContent = localScopeFinalScore;
+
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore > computerScore) {
+            display.textContent = 'You win!!!';
+        }
+        else if (computerScore > humanScore) {
+            display.textContent = 'You lose.';
+            
+        }
+        else {
+            display.textContent = "Draw.";
+        }
+        humanScore = 0;
+        computerScore = 0;
+
+    }
 
     return localScopeFinalScore;
 }
 
+
 // This function calls the playRound function 5 times which means
 // the user gets to play 5 rounds
-// function playGame () {
+// function playGame() {
 //     let scoreBoard;
 //     scoreBoard = playRound(getHumanChoice(), getComputerChoice());
 //     console.log(scoreBoard);
@@ -85,22 +117,9 @@ function playRound(humanChoice, computerChoice) {
 // let finalScore = playGame();
 // console.log("Final score: " + finalScore);
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-
-rock.addEventListener('click', () => {
-    playRound(getHumanChoice('ROCK'),getComputerChoice())
-});
-paper.addEventListener('click', () => {
-    playRound(getHumanChoice('PAPER'),getComputerChoice())
-});
-scissors.addEventListener('click',() => {
-    playRound(getHumanChoice('SCISSORS'),getComputerChoice())
-});
 
 
-const score = document.querySelector('#output');
+
 
 
 
